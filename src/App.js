d393e2d4c5b9e76cd2fded5aaf38cc9952820223
João Pedro Beck Land <joaopedrobeckland@gmail.com>
2019-06-02 17:26:44 -0300

    final front

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import ReduxToastr from 'react-redux-toastr';
+import store from './store';
+import Routes from './routes';
+import GlobalStyle from './styles/global';
+// import { Container } from './styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <>
+      <Routes />
+      <ReduxToastr />
+      <GlobalStyle />
+    </>
+  </Provider>
+);
 
 export default App;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+import TeamSwitcher from '../../components/TeamSwitcher';
+import Projects from '../../components/Projects';
+
+const Main = () => (
+  <Container>
+    <TeamSwitcher />
+    <Projects />
+  </Container>
+);
+
+export default Main;
